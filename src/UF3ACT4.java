5dd4594755d75bc8ebd39d180a04c25e5726d994
Alumne <Alumne@104-PC13>
2019-12-03 18:07:29 +0100

    segon

--- src/UF3ACT4.java
+++ src/UF3ACT4.java
@@ -7,15 +7,19 @@ public class UF3ACT4 {
 		
 		//FLUX PER A ENTRADA ESTÀNDARD
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
 		//Socket client
 		DatagramSocket clientSocket = new DatagramSocket();
 		byte[] enviats = new byte[1024];
 		byte[] rebuts = new byte[1024];		
-			
+		
 		//DADES DEL SERVIDOR al qual s'envia el missatge
 		InetAddress IPServidor = InetAddress.getLocalHost();
 		int port = 9800;
+		boolean tempsNoSuperat = true;
 		
+		while (tempsNoSuperat == true) {
+			
 			//INTRODUIR DADES PEL TECLAT
 			System.out.print("Introdueix missatge: ");
 			String cadena = in.readLine();
@@ -29,16 +33,38 @@ public class UF3ACT4 {
 			//REBENT DATAGRAMA DEL SERVIDOR
 			DatagramPacket rebut = new DatagramPacket(rebuts, rebuts.length);
 			System.out.println("Esperant datagrama...");
-			clientSocket.receive(rebut);
-			String majuscula = new String(rebut.getData());
 			
-			//ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
-			InetAddress IPOrigen = rebut.getAddress();
-			int portOrigen = rebut.getPort();
-			System.out.println("\tProcedent de: "+IPOrigen+":"+portOrigen);
-			System.out.println("\tDades: "+majuscula.trim());
-			//Tanca el socket
-			clientSocket.close();		
+			// Comprobem que el temps no es superi el temps indicat
+			clientSocket.setSoTimeout(5000);
+			try{
+			 //espera de les dades
+				clientSocket.receive(rebut);
+				//processament de les dades rebudes i obtenció de la resposta
+			 }catch(SocketTimeoutException e){
+				 // Si es supera el temps d'espera es mostra el seguent missatge i es cambia el valor
+				 // de tempsNoSuperat a false per finalitzar el programa
+				 System.out.println("Temps d'espera superat, programa finalitzat");
+				 tempsNoSuperat = false;
+			 }
+			
+			if (tempsNoSuperat == true) {
+				
+				String majuscula = new String(rebut.getData());
+				
+				//ACONSEGUINT INFORMACIÓ DEL DATAGRAMA
+				InetAddress IPOrigen = rebut.getAddress();
+				int portOrigen = rebut.getPort();
+				System.out.println("\tProcedent de: "+IPOrigen+":"+portOrigen);
+				System.out.println("\tDades: "+majuscula.trim());
+			}
+			
+			
+			if (tempsNoSuperat == false) {
+				//Tanca el socket
+				clientSocket.close();	
+			}
+				
+		}
 		
 	}
 
